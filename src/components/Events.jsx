c6989a6a923dcef998ff90decbd4e2cd57804c7f
Robin Ruaux <robin.ruaux@gmail.com>
2024-02-16 11:49:17 +0100

    ok end of spins

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -11,25 +11,21 @@ import {eventsPerPage} from "../constants.js";
 function Events({children, disabled, className, loc, ...rest}) {
 
     const [currentEvent, setCurrentEvent] = useState('');
-    const [isPending, setIsPending] = useState(false);
 
     const queryFnInfinite = (page) => {
-        setIsPending(true);
         return fetch('/api/events/nearby?page='+page+(loc?'&lat='+loc.lat+'&lng'+loc.lng: '')).then(e => {
-            setIsPending(false);
             return e.json()
-        }).catch(e=> setIsPending(false))
+        })
     }
 
     const infiniteScrollRef = useRef(null)
 
     return (<div className="events-wrapper">
-        <InfiniteScroll className={"content events"} spinner={<div className={"loc-spinner"}></div>} count={eventsPerPage} refreshTime={0} ref={infiniteScrollRef} fetch={queryFnInfinite} renderItem={(e) => {
+        <InfiniteScroll className={"content events"} spinner={<div className={"loc-spinner white"}></div>} count={eventsPerPage} refreshTime={0} ref={infiniteScrollRef} fetch={queryFnInfinite} renderItem={(e) => {
             return <Event data={e} full={e.hash === currentEvent} onShowInfo={(event) => {
                 setCurrentEvent(event.hash);
             }} />
         }} />
-            {isPending && <div className="loc-spinner white"></div>}
         </div>
     )
 }
