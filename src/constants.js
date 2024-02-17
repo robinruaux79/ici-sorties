a5710c6de594b6bb9fd19f93d5e07c309d41ebc9
Robin Ruaux <robin.ruaux@gmail.com>
2024-02-17 20:32:35 +0100

    better look

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,3 @@ export const contactEmail = 'anonympins@gmail.com';
 export const eventsPerPage = 8;
 
 export const maxReportsBeforeStateChange = 3;
-
-// abus de reporting : controle manuel, envoi d'un email
