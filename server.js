449b42ca2b115704a31dd393667eec3f798dc277
Robin Ruaux <robin.ruaux@gmail.com>
2024-02-23 17:15:00 +0100

    maxpoolsize 40

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ if(cluster.isMaster && isProduction){
 
     // Connection URL
     const dbUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017';
-    const client = new MongoClient(dbUrl);
+    const client = new MongoClient(dbUrl, { maxPoolSize: 40 });
 
     // Database Name
     const dbName = 'ici';
